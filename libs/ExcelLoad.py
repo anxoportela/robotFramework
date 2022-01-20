97e1680c95f09ef3c9287722fb4880b28ca6ddc4
Anxo Portela-Insua Blanco <anxo.portelainsuablanco@plexus.es>
2022-01-20 18:10:38 +0100

    Fixed Windows/Unix directory compatibility.

--- libs/ExcelLoad.py
+++ libs/ExcelLoad.py
@@ -1,17 +1,21 @@
 import pandas as pd
+import platform as p
 from robot.libraries.BuiltIn import BuiltIn as b
-from pathlib import Path as p
+from pathlib import Path as f
 
 
 def whoami():
-    tmp = b().get_variable_value("${SUITE SOURCE}").split("/")
+    if p.system() == "Windows":
+        tmp = b().get_variable_value("${SUITE SOURCE}").split("\\")
+    else:
+        tmp = b().get_variable_value("${SUITE SOURCE}").split("/")
     tmp.reverse()
     out = tmp[0].split(".")[0]
     return out
 
 
 def load_data():
-    excel = p('resources/testing.xlsx')
+    excel = f('resources/testing.xlsx')
     df = pd.read_excel(excel)
     for row in df.itertuples():
         if row.TestName == whoami():
