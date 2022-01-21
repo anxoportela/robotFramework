067154d6ffb540bd48b6526bb6ed73275d408a69
Anxo Portela-Insua Blanco <anxo.portelainsuablanco@plexus.es>
2022-01-21 22:35:35 +0100

    Parameterized project variables

--- libs/ExcelLoad.py
+++ libs/ExcelLoad.py
@@ -1,12 +1,11 @@
-import pandas as pd
 import Common as c
+import pandas as pd
 from pathlib import Path as p
-from pathlib import Path as f
 from robot.libraries.BuiltIn import BuiltIn as b
 
 
 def load_data():
-    df = pd.read_excel(f(c.read_cfg('excel_file')), sheet_name=c.read_cfg('excel_sheet'))
+    df = pd.read_excel(p(c.read_cfg('excel_file')), sheet_name=c.read_cfg('excel_sheet'))
     for row in df.itertuples():
         if row.TestName == p(b().get_variable_value("${SUITE SOURCE}")).stem:
             return row
--- libs/ExcelRunner.py
+++ libs/ExcelRunner.py
@@ -1,20 +1,21 @@
-import fnmatch as f
 import os
-import pandas as pd
 import Common as c
-from datetime import datetime as d
+import fnmatch as f
+import pandas as pd
 from pathlib import Path as p
+from datetime import datetime as d
 
 
 def runtests():
     df = pd.read_excel(p(c.read_cfg('excel_file')), sheet_name=c.read_cfg('excel_sheet'))
     for row in df.itertuples():
         if row.Run == "Y":
-            for root, dirs, files in os.walk('testcases'):
+            for root, dirs, files in os.walk(c.read_cfg('tests_folder')):
                 for name in files:
                     if f.fnmatch(name, row.TestName + ".robot"):
-                        os.system("robot -d results/{0} -L TRACE -b debug.log {1}".
-                                  format(d.now().strftime("%Y%m%d_%H%M%S"), os.path.join(root, name)))
+                        os.system("robot -d results/{0} -L {1} -b debug.log {2}".
+                                  format(d.now().strftime("%Y%m%d_%H%M%S"), c.read_cfg('log_info'),
+                                         os.path.join(root, name)))
 
 
 runtests()
