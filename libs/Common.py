e694d46226b80a455ff5247ec09212bbc2d12686
Anxo Portela-Insua Blanco <anxoportela@gmail.com>
2022-01-22 19:26:34 +0100

    Added error handling, auto installation of webdrivers and test execution based on given sheet name

--- libs/Common.py
+++ libs/Common.py
@@ -1,7 +1,37 @@
+import os
+from datetime import datetime as d
 
 
 def read_cfg(cfg):
-    with open('resources/config/application_config.yaml') as f:
-        for line in f:
-            if line.split(': ')[0] == cfg:
-                return line.split(': ')[1].strip()
+    if not os.path.isfile('resources/config/application_config.yaml'):
+        print_error("No configuration file found on 'resources/config/' go to the directory and create a file named "
+                    "'application_config.yaml'")
+        quit()
+    else:
+        with open('resources/config/application_config.yaml') as f:
+            for line in f:
+                if line.startswith(cfg + ':'):
+                    return line.split(':')[1].strip()
+
+
+def d_now(opt):
+    if opt == "l":
+        return d.now().strftime("%H:%M:%S")
+    elif opt == "f":
+        return d.now().strftime("%Y%m%d_%H%M%S")
+
+
+def colorize_text(r, g, b, text):
+    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+
+
+def print_warning(msg):
+    print("[" + d_now("l") + "] " + colorize_text(255, 255, 0, "WARNING:") + msg)
+
+
+def print_error(msg):
+    print("[" + d_now("l") + "] " + colorize_text(255, 0, 0, "ERROR:") + msg)
+
+
+def print_success(msg):
+    print("[" + d_now("l") + "] " + colorize_text(0, 255, 0, "SUCCESS:") + msg)
--- libs/ExcelRunner.py
+++ libs/ExcelRunner.py
@@ -1,21 +1,50 @@
+import glob
 import os
 import Common as c
-import fnmatch as f
 import pandas as pd
+import chromedriver_autoinstaller as ci
+import geckodriver_autoinstaller as gi
 from pathlib import Path as p
-from datetime import datetime as d
+from openpyxl import load_workbook as lw
 
 
 def runtests():
-    df = pd.read_excel(p(c.read_cfg('excel_file')), sheet_name=c.read_cfg('excel_sheet'))
-    for row in df.itertuples():
-        if row.Run == "Y":
-            for root, dirs, files in os.walk(c.read_cfg('tests_folder')):
-                for name in files:
-                    if f.fnmatch(name, row.TestName + ".robot"):
-                        os.system("robot -d results/{0} -L {1} -b debug.log {2}".
-                                  format(d.now().strftime("%Y%m%d_%H%M%S"), c.read_cfg('log_info'),
-                                         os.path.join(root, name)))
+    now = c.d_now("f")
+    wb = lw(p(c.read_cfg('excel_file'), read_only=True))
+    if not c.read_cfg('excel_sheet') in wb.sheetnames:
+        if c.read_cfg('excel_sheet'):
+            c.print_error("No sheet called '{0}' found on '{1}'".
+                          format(c.read_cfg('excel_sheet'), p(c.read_cfg('excel_file')).name))
+            quit()
+        else:
+            c.print_error("Empty variable 'excel_sheet' on '{0}'".format(p(c.read_cfg('config_file')).name))
+            quit()
+    else:
+        df = pd.read_excel(p(c.read_cfg('excel_file')), sheet_name=c.read_cfg('excel_sheet'))
+        if not df.empty:
+            c.print_success("'{0}' sheet loaded successfully".format(c.read_cfg('excel_sheet')))
+            for row in df.itertuples():
+                if row.Run.lower() == "y":
+                    file_test = glob.glob(c.read_cfg('tests_folder') + "/**/" + row.TestName + ".robot")
+                    if file_test:
+                        if row.Device.lower() == "chrome":
+                            ci.install()
+                        elif row.Device.lower() == "firefox":
+                            gi.install()
+                        os.system("robot -d results/{0}/{1} -L {2} -b debug.log {3}".
+                                  format(now, row.TestName, c.read_cfg('log_info'), file_test[0]))
+                    else:
+                        c.print_error("No test file named '{0}' found".format(row.TestName + ".robot"))
+                else:
+                    file_test = glob.glob(c.read_cfg('tests_folder') + "/**/" + row.TestName + ".robot")
+                    if file_test:
+                        c.print_warning("Test suite '{0}' disabled".format(row.TestName))
+                    else:
+                        c.print_error("Test suite '{0}' disabled and '{1}' not found".
+                                      format(row.TestName, row.TestName + ".robot"))
+        else:
+            c.print_error("No testcases found on '{0}' sheet".format(c.read_cfg('excel_sheet')))
+            quit()
 
 
 runtests()
