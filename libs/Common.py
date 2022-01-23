0d47fffe36c377f1ac8f8d57b09983556bcb9f89
Anxo Portela-Insua Blanco <anxoportela@gmail.com>
2022-01-23 20:29:18 +0100

    Added error handling, excel data load and reports and metrics improvements.

--- libs/Common.py
+++ libs/Common.py
@@ -1,6 +1,37 @@
+import os
+from datetime import datetime as d
+from colorama import init
+from termcolor import colored
+
+init(autoreset=True)
+
 
 def read_cfg(cfg):
-    with open('resources/config/application_config.yaml') as f:
-        for line in f:
-            if line.split(': ')[0] == cfg:
-                return line.split(': ')[1].strip()
+    if not os.path.isfile('resources/config/application_config.yaml'):
+        print_error("No configuration file found on 'resources/config/' go to the directory and create a file named "
+                    "'application_config.yaml'")
+        quit()
+    else:
+        with open('resources/config/application_config.yaml') as f:
+            for line in f:
+                if line.startswith(cfg + ':'):
+                    return line.split(':')[1].strip()
+
+
+def d_now(opt):
+    if opt == "l":
+        return d.now().strftime("%H:%M:%S")
+    elif opt == "f":
+        return d.now().strftime("%Y%m%d_%H%M%S")
+
+
+def print_warning(msg):
+    print("[ {0} ] {1}: {2}".format(d_now("l"), colored("WARNING", 'yellow'), msg))
+
+
+def print_error(msg):
+    print("[ {0} ] {1}: {2}".format(d_now("l"), colored("ERROR", 'red'), msg))
+
+
+def print_success(msg):
+    print("[ {0} ] {1}: {2}".format(d_now("l"), colored("SUCCESS", 'green'), msg))
--- libs/ExcelLoad.py
+++ libs/ExcelLoad.py
@@ -2,9 +2,11 @@ import Common as c
 import pandas as pd
 from pathlib import Path as p
 from robot.libraries.BuiltIn import BuiltIn as b
+import urllib3
 
 
 def load_data():
+    urllib3.disable_warnings()
     df = pd.read_excel(p(c.read_cfg('excel_file')), sheet_name=c.read_cfg('excel_sheet'))
     for row in df.itertuples():
         if row.TestName == p(b().get_variable_value("${SUITE SOURCE}")).stem:
